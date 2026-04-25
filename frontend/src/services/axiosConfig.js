9e3de87c2de46ed466b582f54079dab745c405ec
difmo <difmotech@gmail.com>
2026-04-25 22:16:36 +0530

    update live api url

--- frontend/src/services/axiosConfig.js
+++ frontend/src/services/axiosConfig.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://thick-ties-draw.loca.lt/api',
+  headers: {
+    'ngrok-skip-browser-warning': 'true',
+    'bypass-tunnel-reminder': 'true' // For localtunnel
+  }
 });
 
 // Automatically add token to every request
